bb91294c765fb388fb8480eac19b30efed36dc67
ddy05 <dwightdwayne.yu@outlook.com>
2026-04-02 08:46:15 +0800

    Added the Documents page

---
+++ documents/index.php
@@ -0,0 +1,16 @@
+<!-- Documents Page -->
+<!-- Assigned member: Dwight -->
+<?php
+    require_once '../components/head.php';
+    require_once '../components/navbar.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php initializePage("Documents | YanoDASH")?>
+    </head>
+    <body>
+        <?php echo navbar(1)?>
+    </body>
+</html>
